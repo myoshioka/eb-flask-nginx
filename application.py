0511114e48435036a18813be0e3467181e4e199c
myoshioka <y.mako2@gmail.com>
2020-03-31 18:02:01 +0900

    Initial commit

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    user = {'aaa': 'a', 'bbb': 'b', 'ccc': 'c'}
+    return jsonify(user)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
